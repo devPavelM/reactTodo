39f98b8de6e5583e1282983480149077adab9465
devPavelM <MPavelMail@gmail.com>
2021-11-07 13:59:26 +0300

    Добавление и удаление

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, {useRef} from "react";
 import Counter from "./components/Counter";
 import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
 
-    const [likes, setLikes] = React.useState(0)
+    const [posts, setPosts] = React.useState([
+        {id: 1, title: 'JavaScript', body: 'JavaScript язык программирования'},
+        {id: 2, title: 'JavaScript 1', body: 'JavaScript язык программирования'},
+        {id: 3, title: 'JavaScript 2', body: 'JavaScript язык программирования'}
+    ])
 
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
 
     return (
         <div className="App">
-            <div className='post'>
-                <div className='post__content'>
-                    <strong>1. JavaScript</strong>
-                    <div>
-                        JavaScript язык программирования
-                    </div>
-                </div>
-                <div className='post__btns'>
-                    <button>Удалить</button>
-                </div>
-            </div>
+            <PostForm create={createPost}/>
+            {posts.length
+                ? <PostList remove={removePost} posts={posts} title={'Список постов 1'}/>
+                : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
+            }
         </div>
     );
 }
