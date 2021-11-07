063fefe72dae718a1e7363b3f471ac76ea33b824
devPavelM <MPavelMail@gmail.com>
2021-11-07 19:24:17 +0300

    Добавлена фильтрация и сортировка

--- src/App.js
+++ src/App.js
@@ -6,14 +6,29 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
 
     const [posts, setPosts] = React.useState([
-        {id: 1, title: 'JavaScript', body: 'JavaScript язык программирования'},
-        {id: 2, title: 'JavaScript 1', body: 'JavaScript язык программирования'},
-        {id: 3, title: 'JavaScript 2', body: 'JavaScript язык программирования'}
+        {id: 1, title: 'аа', body: 'юю'},
+        {id: 2, title: 'бб 1', body: 'лл'},
+        {id: 3, title: 'юю 2', body: 'вв'}
     ])
+
+    const [filter, setFilter] = React.useState({sort: '', query: ''})
+
+    const sortedPosts = React.useMemo(() => {
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = React.useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -26,10 +41,9 @@ function App() {
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            {posts.length
-                ? <PostList remove={removePost} posts={posts} title={'Список постов 1'}/>
-                : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
-            }
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter filter={filter} setFilter={setFilter}/>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
         </div>
     );
 }
